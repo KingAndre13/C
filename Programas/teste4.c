a3576fd051429123dfe8d91967f981f530d57c2a
André Diogo <andrediogods@gmail.com>
2025-03-01 09:41:55 -0300

    Exercícios da Faculdade

---
+++ Programas/teste4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main () {
+    int idade;
+    int filhos = 2;
+    float preço = 20.35;
+    double peso = 90.3;
+    char NomeDosFilhos = 'Fabio';
+    char nome[20] = "Sergio";
+
+    idade = 25;
+    filhos = 4;
+}
+
---
+++ Programas/teste5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+    char nome [20];
+    int idade;
+    printf("Digite o seu primeiro nome: ");
+    scanf("%s", nome);
+    printf("Digite sua idade: ");
+    scanf("%d", &idade);
+    printf("Seu nome e %s e voce tem %d anos ", nome, idade);
+}
